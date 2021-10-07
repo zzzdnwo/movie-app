c344a80c0f9fbca07c85d7792a32e4ef21d6dc9c
zzzdnwo <swjdnwo@gmail.com>
2021-10-07 10:37:59 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 
-function Food(){
-  return <h1>Food1</h1>;
+function Food({favourite}){
+  return <h1>I like {favourite}</h1>;
 }
 
 function App() {
   return (
     <>
       <h1>Hello</h1>
-      <Food fav="chicken" />
+      <Food favourite="chicken" />
+      <Food favourite="juice" />
+      <Food favourite="cola" />
+      <Food favourite="cider" />
     </>
   )
 }
 
 export default App;
+ 
